f91ce43373cc5b837f9c5d1e0a3466e0b5f7cc03
rwbu69 <lovelyrwbu69@gmail.com>
2025-12-10 02:20:31 +0700

    Fix all asset paths to use base URL for GitHub Pages

--- public/scripts/quiz.js
+++ public/scripts/quiz.js
@@ -1,7 +1,11 @@
 import { CharacterQuiz } from './modules/CharacterQuiz.js';
 
+// Get the base path from the current script location
+const scriptPath = new URL(import.meta.url).pathname;
+const basePath = scriptPath.includes('/gubuk-trainer/') ? '/gubuk-trainer' : '';
+
 // Fetch character data from static JSON file (works with GitHub Pages)
-fetch('/data/characters-en.json')
+fetch(`${basePath}/data/characters-en.json`)
   .then(response => response.json())
   .then(data => {
     const quiz = new CharacterQuiz(data.characters, data.uniqueCharacters);
